11f8648a9ba3c6eb04dcc308c716d14032270355
Snehamohan2k <snehamohan2k@gmail.com>
2023-03-22 17:54:56 +0530

    Progress bar

--- src/component/VerifyProgress.js
+++ src/component/VerifyProgress.js
@@ -1,12 +1,11 @@
 import React from "react";
 import {  Progress, Space, Typography } from "antd";
 import { FileOutlined } from '@ant-design/icons';
-
-function VerifyProgress({ uploadProgress }) {
+  const VerifyProgress=(props)=>{
   return (
     <div>
       <div>
-        {Object.keys(uploadProgress).map((fileName) => (
+        {Object.keys(props.uploadProgress).map((fileName) => (
           <div key={fileName}>
             <Space
               direction="vertical"
@@ -16,14 +15,14 @@ function VerifyProgress({ uploadProgress }) {
                 <FileOutlined />
                 <span>{fileName}</span>
                 <Typography.Text type="secondary">
-                  {uploadProgress[fileName] &&
-                  uploadProgress[fileName].estimatedTime !== undefined
-                    ? ` is being verified in ${uploadProgress[fileName].estimatedTime} seconds`
+                  {props.uploadProgress[fileName] &&
+                  props.uploadProgress[fileName].estimatedTime !== undefined
+                    ? ` is being verified in ${props.uploadProgress[fileName].estimatedTime} seconds`
                     : ""}
                 </Typography.Text>
               </Space>
               <Progress
-                percent={Math.ceil(uploadProgress[fileName].progress)}
+                percent={Math.ceil(props.uploadProgress[fileName].progress)}
               />
             </Space>
           </div>
